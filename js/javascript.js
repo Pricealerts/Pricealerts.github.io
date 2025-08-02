cb831137cb0feb6b207a0bd77fcdb59d9cf333c7
stocksManagement <slimani.abdelhadi08@gamil.com>
2025-08-03 00:09:02 +0100

    qsd

--- js/javascript.js
+++ js/javascript.js
@@ -13,7 +13,6 @@ let allPrices = [];
 const conditionGreaterThanOrEqual = gebi(
 	"conditionGreaterThanOrEqual"
 );  */
-/* dadi */
 const alertTypeBrowserCheckbox = gebi("alertTypeBrowser"); // تم تغيير الاسم
 const alertTypeTelegramCheckbox = gebi("alertTypeTelegram"); // تم تغيير الاسم
 const telegramChatIdContainer = gebi("telegramChatIdContainer");
