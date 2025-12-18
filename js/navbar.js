7ef4b15c5f4cef74ff5b4f12866b12f1f081aa4f
stocksManagement <slimani.abdelhadi08@gamil.com>
2025-12-18 19:17:19 +0100

    qdfsdf

--- js/navbar.js
+++ js/navbar.js
@@ -2,25 +2,24 @@ let UserImage = "/imgs/web/apple-touch-icon.png";
 let userNameDirction = localStorage.userName || "حسابي";
 let enteEsc = ` تسجبل الدخول
  <img src="/imgs/web/signin-svgrepo-com.svg" alt="">`;
-let dsplyCont = 'display: none;'
+let dsplyCont = "display: none;";
 if (localStorage.base64Pctr) {
 	UserImage = localStorage.base64Pctr;
 	enteEsc = ` تسجبل الخروج
  <img src="/imgs/web/signout-svgrepo-com.svg" alt="">`;
- dsplyCont = 'display: flex;'
+	dsplyCont = "display: flex;";
 }
- let enteEsc2 = `
+let enteEsc2 = `
  حسابي
  <img src=${UserImage} alt="">
  `;
 
 function exsit() {
 	if (localStorage.email) {
-		localStorage.removeItem('base64Pctr');
+		localStorage.removeItem("base64Pctr");
 	}
 	window.location.href = "https://pricealerts.web.app/signin";
 }
-
 
 /* 
 width: auto;
@@ -34,34 +33,36 @@ width: auto;
 
 //nta3 navbar saide bar
 
-
 const sidebar = document.createElement("div");
 //sidebar.id = 'sidebar';   #1c274c
 sidebar.className = "sidebar";
 sidebar.innerHTML = `<!-- Sidebar (LEFT) -->
 			<h3>القائمة</h3>
 			<a href="https://pricealerts.web.app/">
-			الرئيسية
-			<img src="/imgs/web/home-alt-1-svgrepo-com.svg" alt="home"></a>
+				الرئيسية
+				<img src="/imgs/web/home-alt-1-svgrepo-com.svg" alt="home"></a>
 			<a href="https://pricealerts.web.app/otherPage/about.html">
-			من نحن
-             <img src="/imgs/web/info-circle-svgrepo-com.svg" alt="inf"></a>
+				من نحن
+            	<img src="/imgs/web/info-circle-svgrepo-com.svg" alt="inf"></a>
 
 		<!--
         	<a href="#">المنتجات</a>
 			<a href="#">الخدمات</a> 
              -->
 			<a href="https://pricealerts.web.app/otherPage/privacy.html">
-            سياسة الخصوصية
-			<img src="/imgs/web/privacy-svgrepo-com.svg" alt=""></a>
+           		سياسة الخصوصية
+				<img src="/imgs/web/privacy-svgrepo-com.svg" alt=""></a>
 			<a href="https://pricealerts.web.app/otherPage/contact.html">
             اتصل بنا
-			<img src="/imgs/web/mail-svgrepo-com.svg" alt="home"></a>
+				<img src="/imgs/web/mail-svgrepo-com.svg" alt="home"></a>
 			<a id="signOutOrInLink" style="cursor:pointer"  onclick = "exsit()">
-       ${enteEsc}</a>
-	   <a id="accountLink" href="https://pricealerts.web.app/accont"  style = "${dsplyCont} width: auto; height: 35px;">
-       ${userNameDirction}
-	   <img src="${UserImage}" alt=""></a>
+        		${enteEsc}
+			</a>
+			<a id="accountLink" href="https://pricealerts.web.app/accont"
+				style = "${dsplyCont} width: auto; height: 35px;border-radius: 50%;">
+				${userNameDirction}
+				<img src="${UserImage}" alt="">
+			</a>
 `;
 
 document.body.prepend(sidebar);
